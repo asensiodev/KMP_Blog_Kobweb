f2c27e142e3c57812e72a6d034d86cef241d8d74
asensiodev <asensiodev@gmail.com>
2024-03-02 00:01:13 +0100

    Add button

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/admin/Login.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/admin/Login.kt
@@ -1,8 +1,10 @@
 package com.example.blogmultiplatform.pages.admin
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import com.example.blogmultiplatform.util.Res
 import com.models.Theme
+import com.varabyte.kobweb.compose.css.FontSize
+import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
@@ -13,25 +15,33 @@ import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.text.SpanText
 import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.Input
 
 @Page
 @Composable
 fun LoginScreen() {
 
+    var errorText by remember { mutableStateOf("") }
+
     Box(
         modifier = Modifier.fillMaxSize().backgroundColor(Theme.Outline.rgb),
         contentAlignment = Alignment.Center
     ) {
         Column(
-            modifier = Modifier.padding(leftRight = 50.px, bottom = 24.px),
+            modifier = Modifier.padding(topBottom = 24.px, leftRight = 50.px),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                modifier = Modifier.width(100.px).margin(bottom = 50.px),
+                modifier = Modifier
+                    .width(100.px)
+                    .margin(bottom = 30.px)
+                    .borderRadius(5.px),
                 src = Res.Image.LOGO,
                 description = "Logo Image"
             )
@@ -43,6 +53,8 @@ fun LoginScreen() {
                     .padding(leftRight = 20.px)
                     .margin(bottom = 12.px)
                     .backgroundColor(Colors.White)
+                    .border(width = 0.px, style = LineStyle.None, color = Colors.Transparent)
+                    .borderRadius(10.px)
                     .toAttrs {
                         attr(attr = "placeholder", value = "Username")
                     }
@@ -53,10 +65,36 @@ fun LoginScreen() {
                     .width(350.px)
                     .height(54.px)
                     .padding(leftRight = 20.px)
+                    .margin(bottom = 20.px)
                     .backgroundColor(Colors.White)
+                    .border(width = 0.px, style = LineStyle.None, color = Colors.Transparent)
+                    .borderRadius(10.px)
                     .toAttrs {
                         attr(attr = "placeholder", value = "Password")
                     }
+            )
+            Button(
+                attrs = Modifier
+                    .width(100.px)
+                    .backgroundColor(Theme.Primary.rgb)
+                    .color(Colors.White)
+                    .borderRadius(20.px)
+                    .fontSize(FontSize.Medium)
+                    .border(width = 2.px, style = LineStyle.None, color = Colors.White)
+
+                    .padding(10.px)
+                    .toAttrs()
+            ) {
+                SpanText(
+                    text = "Sign in",
+                    modifier = Modifier
+                )
+            }
+            SpanText(
+                text = errorText,
+                modifier = Modifier
+                    .color(Colors.Red)
+                    .textAlign(TextAlign.Center)
             )
         }
     }
