cf7756e455c5036ce5d62931137235f7f81dc123
asensiodev <asensiodev@gmail.com>
2024-04-26 13:51:03 +0200

    Add side panel options

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/components/SidePanel.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/components/SidePanel.kt
@@ -3,11 +3,19 @@ package com.example.blogmultiplatform.components
 import androidx.compose.runtime.Composable
 import com.example.blogmultiplatform.util.Res
 import com.example.blogmultiplatform.models.Theme
+import com.example.blogmultiplatform.util.Constants.ROBOTO
+import com.varabyte.kobweb.compose.css.Cursor
+import com.varabyte.kobweb.compose.dom.svg.Path
+import com.varabyte.kobweb.compose.dom.svg.Svg
 import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.text.SpanText
+import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.Position
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.css.vh
@@ -29,8 +37,63 @@ fun SidePanel() {
             alt = "Logo"
         )
         SpanText(
-            modifier = Modifier.fontSize(14.px),
+            modifier = Modifier
+                .margin(30.px)
+                .fontSize(14.px)
+                .color(Theme.HalfWhite.rgb),
             text = "Dashboard"
         )
+        NavigationItem(
+            title = "Home",
+            iconPath = Res.PathIcon.home,
+            onClick = {}
+        )
+        NavigationItem(
+            title = "Create",
+            iconPath = Res.PathIcon.home,
+            onClick = {}
+        )
+        NavigationItem(
+            title = "Posts",
+            iconPath = Res.PathIcon.home,
+            onClick = {}
+        )
+        NavigationItem(
+            title = "Logout",
+            iconPath = Res.PathIcon.home,
+            onClick = {}
+        )
     }
+}
+
+@Composable
+fun NavigationItem(
+    modifier: Modifier = Modifier,
+    selected: Boolean = false, // TODO: Change icon and text color upon this
+    title: String,
+    iconPath: String,
+    onClick: () -> Unit
+) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .cursor(Cursor.Pointer)
+            .onClick { onClick() }
+    ) {
+        SidePanelIcon(iconPath)
+        SpanText(
+            text = title,
+            modifier = Modifier.color(Color.white)
+        )
+    }
+}
+
+@Composable
+fun SidePanelIcon(iconPath: String) {
+    Image(
+        modifier = Modifier
+            .width(30.px)
+            .margin(right = 5.px, bottom = 10.px)
+            .borderRadius(5.px),
+        src = iconPath
+    )
 }
--- site/src/jsMain/kotlin/com/example/blogmultiplatform/util/Constants.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/util/Constants.kt
@@ -1,12 +1,19 @@
 package com.example.blogmultiplatform.util
 
 object Constants {
-
+    const val ROBOTO = "Roboto"
 }
 
 object Res {
     object Image {
         const val LOGO = "/logo.png"
+    }
+
+    object PathIcon {
+        const val home = "/plus.png"
+        const val create = "/plus.png"
+        const val posts = "/plus.png"
+        const val logout = "/plus.png"
     }
 }
 
